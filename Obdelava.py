77bf0a11c968bfdef1f643bbfeb6fe2fe25d514e
Julija Meglič <jm70582@student.uni-lj.si>
2024-07-20 18:22:08 +0200

    obdelava spisano

--- Obdelava.py
+++ Obdelava.py
@@ -9,5 +9,34 @@ def preberi_dat_v_niz(directory, ime):
         text = file_in.read()
     return text
 
-def stran_v_oglase(vsebina_strani):
-    pass
+def stran_v_recepte(vsebina_strani):
+    return re.findall(r'{"title":*?,"headingStyledSize"*?}]},', vsebina_strani, flags=re.DOTALL)
+
+def recept_v_slovar(recept):
+    ime = re.search(r'{"title":"(.*?)","headingStyledSize"', recept)
+    id = re.search(r'"id":"(.*?)"', recept)
+    ocena = re.search(r'{"ratingValue":(.*?),', recept)
+    st_ocen = re.search(r'"ratingCount":(.*?),', recept)
+    cas_priprave = re.search(r'{"slug":"time","display":"(.*?)"}', recept)
+    level = re.search(r'{"slug":"skillLevel","display":"(.*?)"}', recept)
+    healthy = re.search(r'{"slug":"healthy","display":"Healthy"}', recept)
+    vegetarian = re.search(r'{"slug":"vegetarian","display":"Vegetarian"}', recept)
+    gluten_free = re.search(r'{"slug":"gluten-free","display":"Gluten-free"}', recept)
+    vegan = re.search(r'{"slug":"vegan","display":"Vegan"}', recept)
+
+    #if ocena == None or st_ocen == None or heatlhy == None or vegetarian == None or gluten_free == None or vegan == None:
+    #    return None
+    sez = [healthy, vegetarian, gluten_free, vegan]
+    for el in sez:
+        if el == None:
+            el = False
+        else:
+            el = True
+    
+    return {'ime': ime.group(1), 'id': id.group(1), 'ocena': ocena.group(1), 'st_ocen': st_ocen.group(1), 'cas_priprave': cas_priprave.group(1), 'level': level.group(1), 'healthy': healthy, 'vegetarian': vegetarian, 'gluten_free': gluten_free, 'vegan': vegan}
+
+def recepte_v_datoteko(ime, directory):
+    vsebina_strani = preberi_dat_v_niz(directory, ime)
+    recept = stran_v_recepte(vsebina_strani)
+    recepti = [recept_v_slovar(kos) for kos in recept]
+    return [rec for rec in recepti]
